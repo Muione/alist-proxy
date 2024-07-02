b248b4f584cf579950508b38650b23b1049908c6
Muione <2541788981@qq.com>
2024-07-02 22:13:11 +0800

    更改配置信息传入方式，使用config.yaml传入配置

--- alist-proxy.go
+++ alist-proxy.go
@@ -7,9 +7,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/alist-org/alist/v3/pkg/sign"
+	"gopkg.in/yaml.v2"
 )
 
 type Link struct {
@@ -24,24 +27,70 @@ type LinkResp struct {
 }
 
 var (
-	port              int
-	https             bool
-	help              bool
-	certFile, keyFile string
-	address, token    string
-	s                 sign.Sign
+	help       bool
+	configFile string
+	s          sign.Sign
 )
 
+var config struct {
+	Port     int    `yaml:"port"`
+	Https    bool   `yaml:"https"`
+	Help     bool   `yaml:"help"`
+	CertFile string `yaml:"certFile"`
+	KeyFile  string `yaml:"keyFile"`
+	Address  string `yaml:"address"`
+	Token    string `yaml:"token"`
+}
+
+func loadConfig(filename string) error {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func init() {
-	flag.IntVar(&port, "port", 5243, "the proxy port.")
-	flag.BoolVar(&https, "https", false, "use https protocol.")
-	flag.BoolVar(&help, "help", false, "show help")
-	flag.StringVar(&certFile, "cert", "server.crt", "cert file")
-	flag.StringVar(&keyFile, "key", "server.key", "key file")
-	flag.StringVar(&address, "address", "", "alist address")
-	flag.StringVar(&token, "token", "", "alist token")
+	flag.BoolVar(&help, "h", false, "help")
+	flag.BoolVar(&help, "help", false, "help")
+	flag.StringVar(&configFile, "c", "config.yaml", "path to config.yaml")
 	flag.Parse()
-	s = sign.NewHMACSign([]byte(token))
+	err := loadConfig(configFile)
+	if err != nil {
+		// 如果配置文件不存在，则在当前目录下创建一个默认配置文件
+		if os.IsNotExist(err) {
+			fmt.Println("config.yaml not found, create a default config file")
+			defaultConfig := `# the proxy port
+port: 5243
+# use https protocol
+https: false
+# https cert file
+certFile: server.crt
+# https key file
+keyFile: server.key
+# alist server address
+address: http://example.com
+# alist server api token
+token: alist-xxx
+`
+			err := os.WriteFile("config.yaml", []byte(defaultConfig), 0644)
+			if err != nil {
+				fmt.Printf("Create config file error: %s\n", err.Error())
+				os.Exit(1)
+			}
+			fmt.Println("Create config file success, please edit it and restart the proxy!")
+			os.Exit(0)
+		}
+		fmt.Printf("load config error: %s\n", err.Error())
+		os.Exit(1)
+	}
+	s = sign.NewHMACSign([]byte(config.Token))
 }
 
 var HttpClient = &http.Client{}
@@ -65,6 +114,16 @@ func downHandle(w http.ResponseWriter, r *http.Request) {
 	filePath := r.URL.Path
 	err := s.Verify(filePath, sign)
 	if err != nil {
+		// 签名验证失败，写入日志，格式为：
+		// [error] host:port - [date] - error - method - proto - path
+		fmt.Printf("[error] %s - [%s] - %s - %s - %s - %s\n",
+			r.RemoteAddr,
+			time.Now().Format("02/Jan/2006:15:04:05 +0800"),
+			err.Error(),
+			r.Method,
+			r.Proto,
+			r.URL.Path,
+		)
 		errorResponse(w, 401, err.Error())
 		return
 	}
@@ -72,9 +131,9 @@ func downHandle(w http.ResponseWriter, r *http.Request) {
 		"path": filePath,
 	}
 	dataByte, _ := json.Marshal(data)
-	req, _ := http.NewRequest("POST", fmt.Sprintf("%s/api/fs/link", address), bytes.NewBuffer(dataByte))
+	req, _ := http.NewRequest("POST", fmt.Sprintf("%s/api/fs/link", config.Address), bytes.NewBuffer(dataByte))
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", token)
+	req.Header.Set("Authorization", config.Token)
 	res, err := HttpClient.Do(req)
 	if err != nil {
 		errorResponse(w, 500, err.Error())
@@ -101,11 +160,15 @@ func downHandle(w http.ResponseWriter, r *http.Request) {
 	if !strings.HasPrefix(resp.Data.Url, "http") {
 		resp.Data.Url = "http:" + resp.Data.Url
 	}
-	fmt.Println("proxy:", resp.Data.Url)
-	if err != nil {
-		errorResponse(w, 500, err.Error())
-		return
-	}
+	// 请求链接成功，写入日志：
+	// [error] host:port - [date] - error - method - proto - path
+	fmt.Printf("[info] %s - [%s] - - %s - %s - %s\n",
+		r.RemoteAddr,
+		time.Now().Format("02/Jan/2006:15:04:05 +0800"),
+		r.Method,
+		r.Proto,
+		r.URL.Path,
+	)
 	req2, _ := http.NewRequest(r.Method, resp.Data.Url, nil)
 	for h, val := range r.Header {
 		req2.Header[h] = val
@@ -142,18 +205,18 @@ func main() {
 		flag.Usage()
 		return
 	}
-	addr := fmt.Sprintf(":%d", port)
-	fmt.Printf("listen and serve: %s\n", addr)
+	addr := fmt.Sprintf(":%d", config.Port)
+	fmt.Printf("listening on port: %s\n", addr)
 	s := http.Server{
 		Addr:    addr,
 		Handler: http.HandlerFunc(downHandle),
 	}
-	if !https {
+	if !config.Https {
 		if err := s.ListenAndServe(); err != nil {
 			fmt.Printf("failed to start: %s\n", err.Error())
 		}
 	} else {
-		if err := s.ListenAndServeTLS(certFile, keyFile); err != nil {
+		if err := s.ListenAndServeTLS(config.CertFile, config.KeyFile); err != nil {
 			fmt.Printf("failed to start: %s\n", err.Error())
 		}
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Xhofe/alist-proxy
 
 go 1.19
 
-require github.com/alist-org/alist/v3 v3.0.0-beta.0 // indirect
+require (
+	github.com/alist-org/alist/v3 v3.0.0-beta.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0
+)
